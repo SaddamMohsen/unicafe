4649fde40ee9e16e7ea774499542711b169f97fb
SaddamMohsen <sad.2000@outlook.com>
2019-10-25 16:39:13 +0300

    Solution of Exrcise from 1.8 to 1.10

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-const-assign */
 import React from 'react';
 import  { useState } from 'react';
 
@@ -7,53 +8,45 @@ import './App.css';
 
 const Button =(probs)=>( <button onClick={probs.handleClick}>{probs.text}</button>)
 
-/*const GetAverage =({probs})=>
-{const [average,setAverage] =useState(0)
-  
- setAverage((probs.good-probs.bad)/probs.all);
-
-return(<div>{average}</div>
-)
-
-}*/
-function GetAverage(average,good,bad,all)
-{
-  
-     average=(good-bad)/all;
-     return average;
+const Statistics = (probs)=>{
+      if(probs.allva !== 0){
+  return(
+    <div>
+      <table>
+         <tr><td>good</td><td>{probs.good}</td> </tr> 
+         <tr> <td>neutral</td><td> {probs.neutral}</td> </tr>
+         <tr>  <td> bad</td><td>{probs.bad}</td> </tr>
+         <tr><td> All</td><td>{probs.allva}</td> </tr> 
+         <tr><td>Average</td><td>{(probs.good-probs.bad)/probs.allva}% </td></tr>
+         <tr><td>Possitive Avg</td><td>{probs.good/probs.allva}%</td></tr>
+         </table>
+         </div>)
+      }
+      return(
+      <h3>No feedback Given</h3>
+      )
 }
-
-const App = () => {
+  
+  const App =()=> {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allva,setAll] = useState(0)
-  const [average,setAverage] = useState(0);
-
- /* const handleAverage=()=>
-  {
-    setAverage(state=>({average:(good-bad)/all}));
-  }*/
-
-
-const handleGood=()=>{
   
+const handleGood=()=>{
   setAll(allva+1);
   setGood(good+1);
-  setAverage(GetAverage(average,good,bad,allva))
 }
 
 const handleNeutral=()=>{
 setAll(allva+1);
 setNeutral(neutral+1)
-setAverage(GetAverage(average,good,bad,allva))
 }
 
 const handleBad=()=>{
   setAll(allva+1);
   setBad(bad+1)
-  setAverage(GetAverage(average,good,bad,allva))
   }
   return (
     <div className='App-header'>
@@ -62,10 +55,7 @@ const handleBad=()=>{
     <Button handleClick={handleNeutral} text='Neutral'/>
     <Button handleClick={handleBad} text='Bad'/>
     <h2>Statistics</h2>
-      <br/>
-         good={good}  <br/> neutral= {neutral} <br/>    bad={bad}
-         <br/> All={allva} <br/> Average={average}
-         
+    <Statistics good={good} bad={bad} neutral={neutral} allva={allva} />
     </div>
   )
 }
